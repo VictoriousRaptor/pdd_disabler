5e2de8dce36910afb1e0af71358be62943faa8f9
reigadegr <272268642@qq.com>
2024-12-08 21:40:54 +0800

    prev fix

--- src/run/run_cmd.rs
+++ src/run/run_cmd.rs
@@ -1,4 +1,4 @@
-//From ab/SummerSK/puff/Aestas.Britannia
+use anyhow::Result;
 use log::info;
 use std::process::Command;
 pub fn kill_app(package_name: &str) {
@@ -6,21 +6,15 @@ pub fn kill_app(package_name: &str) {
     let _ = Command::new("sh").args(["-c", &cmdline]).output();
 }
 
-pub fn precise_kill(package_name: &str) {
+pub fn precise_kill(package_name: &str) -> Result<()> {
     let cmdline = format!("ps -ef | grep {} | grep -v grep | wc -l", package_name);
-    let output = Command::new("sh").args(["-c", &cmdline]).output().unwrap();
-    if output.clone().status.success() {
-        // 将输出转换为字符串并打印
-        let output_str: &str = &String::from_utf8_lossy(&output.stdout);
-        let output_str = output_str.trim();
-        let output_str: u8 = output_str.parse().unwrap_or_default();
-        if output_str > 0 && output_str < 4 {
-            info!("清算{}", package_name);
-            kill_app(package_name);
-        }
-    } else {
-        // 如果命令执行失败，打印错误信息
-        let error_str = String::from_utf8_lossy(&output.stderr);
-        info!("Error: {}", error_str);
+    let output = Command::new("sh").args(["-c", &cmdline]).output()?;
+    // 将输出转换为字符串并打印
+    let output_str: &str = &String::from_utf8_lossy(&output.stdout);
+    let output_str = output_str.trim();
+    let output_str: u8 = output_str.parse()?;
+    if output_str > 0 && output_str < 4 {
+        kill_app(package_name);
     }
+    Ok(())
 }
--- src/run/start.rs
+++ src/run/start.rs
@@ -1,11 +1,12 @@
 use crate::run::run_cmd::precise_kill;
 use crate::shared::get_top_app::get_topapp_pid_and_name;
 use anyhow::Result;
+use log::info;
 use std::thread;
 use tokio::time::Duration;
 pub async fn thread_start() -> Result<()> {
     // 启动运行线程任务
-    let run_thread = tokio::spawn(async move { app_run() });
+    let run_thread = tokio::spawn(async move { app_run("com.xunmeng.pinduoduo") });
 
     // 等待
     let _ = run_thread.await?;
@@ -13,7 +14,7 @@ pub async fn thread_start() -> Result<()> {
     Ok(())
 }
 
-fn app_run() -> Result<()> {
+fn app_run(package_name: &str) -> Result<()> {
     let mut global_package = String::new();
     loop {
         let (_, name) = get_topapp_pid_and_name()?;
@@ -23,10 +24,15 @@ fn app_run() -> Result<()> {
         }
         global_package = name;
         // 如果此次启动的APP为拼多多，则不kill拼多多
-        if global_package == "com.xunmeng.pinduoduo" {
+        if global_package == package_name {
             continue;
         }
         // 用户删除拼多多后台时自动清理拼多多残留进程
-        precise_kill("com.xunmeng.pinduoduo");
+        let rs = precise_kill(package_name);
+        if rs.is_err() {
+            info!("清算失败: {}", package_name);
+            continue;
+        }
+        info!("清算成功: {}", package_name);
     }
 }
