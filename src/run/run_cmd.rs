9adb0c517744a7181eccd97a91d25e3d2fab3e6a
reigadegr <272268642@qq.com>
2024-12-08 21:22:16 +0800

    prev fix

--- src/run/run_cmd.rs
+++ src/run/run_cmd.rs
@@ -14,7 +14,7 @@ pub fn precise_kill(package_name: &str) {
         let output_str: &str = &String::from_utf8_lossy(&output.stdout);
         let output_str = output_str.trim();
         let output_str: u8 = output_str.parse().unwrap_or_default();
-        if output_str > 0 && output_str < 4 {
+        if output_str < 4 {
             info!("清算{}", package_name);
             kill_app(package_name);
         }
