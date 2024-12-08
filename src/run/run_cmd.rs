30d870951a760e2b897265d1f2f96a1705a0bd75
reigadegr <272268642@qq.com>
2024-12-08 13:22:11 +0800

    prev fix

--- src/run/run_cmd.rs
+++ src/run/run_cmd.rs
@@ -2,19 +2,13 @@
 use log::info;
 use std::process::Command;
 pub fn kill_app(package_name: &str) {
-    let _ = Command::new("nohup")
-        .args(["am", "force-stop", package_name, ">/dev/null", "2>&1", "&"])
-        .output();
+    let cmdline = format!("am force-stop {}", package_name);
+    let _ = Command::new("sh").args(["-c", &cmdline]).output();
 }
 
 pub fn precise_kill(package_name: &str) {
-    let output = Command::new("sh")
-        .args([
-            "-c",
-            "ps -ef | grep com.xunmeng.pinduoduo | grep -v grep | wc -l",
-        ])
-        .output()
-        .unwrap();
+    let cmdline = format!("ps -ef | grep {} | grep -v grep | wc -l", package_name);
+    let output = Command::new("sh").args(["-c", &cmdline]).output().unwrap();
     if output.clone().status.success() {
         // 将输出转换为字符串并打印
         let output_str: &str = &String::from_utf8_lossy(&output.stdout);
@@ -27,6 +21,6 @@ pub fn precise_kill(package_name: &str) {
     } else {
         // 如果命令执行失败，打印错误信息
         let error_str = String::from_utf8_lossy(&output.stderr);
-        println!("Error: {}", error_str);
+        info!("Error: {}", error_str);
     }
 }
